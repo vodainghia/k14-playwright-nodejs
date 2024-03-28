4097752e153bced31764317c18ff25b3507d3da4
NghiaVo <nghiadaivo@gmail.com>
2024-03-28 20:10:59 +0700

    how to use fixture to replace creating page

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -1,15 +1,9 @@
 import { chromium, Browser, Page, BrowserContext } from 'playwright';
 import { test } from '@playwright/test';
 
-test('Login Test', async () => {
-    const browser: Browser = await chromium.launch();
-    const context: BrowserContext = await browser.newContext();
-    const page: Page = await context.newPage();
-
+test('Login Test', async ({page}) => {
     await page.goto('https://playwright.dev');
 
     // Debug only
     await page.waitForTimeout(1000);
-
-    await browser.close();
 });
