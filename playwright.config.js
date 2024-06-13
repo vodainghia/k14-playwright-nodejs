b4e3cb5b91d923c92f7d7805de327d0cfeadfca3
NghiaVo <nghiadaivo@gmail.com>
2024-06-14 03:08:15 +0700

    day30: Allure Report

--- playwright.config.js
+++ playwright.config.js
@@ -1,16 +1,19 @@
-const { defineConfig, devices } = require('@playwright/test');
+const {defineConfig, devices} = require('@playwright/test');
 
 module.exports = defineConfig({
     testDir: './tests',
     projects: [
         {
             name: 'chromium',
-            use: { ...devices['Desktop Chrome'] },
+            use: {...devices['Desktop Chrome']},
         },
     ],
-    reporter: 'html',
+    reporter: [
+        ['html'],
+        ['allure-playwright'],
+    ],
     // if CI environment then retry 2 times, else: retry 1 times 
-    retries: process.env.CI ? 2 : 1,
+    //retries: process.env.CI ? 2 : 1,
     use: {
         actionTimeout: 5 * 1000, // the same as implicit wait in Selenium
         trace: 'on-first-retry',
